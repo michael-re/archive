89fecf231f05abc3e1df4258d8d57d40fb81b27f
michael-re <102764781+michael-re@users.noreply.github.com>
2024-11-25 20:04:11 -0800

    feat: implement `lex_whitespace`

--- archive/source/archive/frontend/lexer.cpp
+++ archive/source/archive/frontend/lexer.cpp
@@ -17,10 +17,38 @@ auto Lexer::lex() -> Token
     utility::ignore(m_source++);
     return (!current)
          ? Token(Token::Type::EndOfFile, location)
-         : Token(Token::Type::Unknown,   location);
+         : Token(Token::Type::Unknown,   location, { *current, '\0' });
 }
 
 auto Lexer::lex_whitespace() -> std::optional<char>
 {
-    return std::optional<char>();
+    while (m_source[0])
+    {
+        // whitespace - any non printable ascii characters + space (' ')
+        if (m_source[0] <= 0x20)
+        {
+            utility::ignore(m_source++);
+            continue;
+        }
+
+        // single-line comments
+        if (m_source.consume("//"))
+        {
+            while (m_source[0] && !m_source.consume('\n'))
+                utility::ignore(m_source++);
+            continue;
+        }
+
+        // multi-line comments
+        if (m_source.consume("/*"))
+        {
+            while (m_source[0] && !m_source.consume("*/"))
+                utility::ignore(m_source++);
+        }
+
+        // non whitespace character
+        break;
+    }
+
+    return m_source[0];
 }
